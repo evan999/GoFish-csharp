9a87439a484fcc97772ac35a30994a6af7c76ecf
evan999 <emeshberge@gmail.com>
2019-12-09 19:18:16 -0500

    Update Hand class.

--- GoFish/Hand.cs
+++ GoFish/Hand.cs
@@ -7,9 +7,29 @@ namespace GoFish
     public class Hand
     {
         List<Card> hand = new List<Card>();
+        Random random = new Random();
 
         public List<Card> DealHandToPlayer(List<Card> hand)
         {
+            for(int card = 0; card < 5; card++)
+            {
+                hand.Add(new Card(random.Next(1, 14), random.Next());
+            }
+            return hand;
+        }
+
+        public void DrawCardFromHand(List<Card> hand)
+        {
+            Console.WriteLine("Draw a card from your hand.");
+            Console.WriteLine("Enter card face value");
+            string cardDrawnFace = Console.ReadLine();
+            Console.WriteLine("Enter card suit");
+            string cardDrawnSuit = Console.ReadLine();
+
+            int cardFace = Convert.ToInt32(cardDrawnFace);
+            int cardSuit = Convert.ToInt32(cardDrawnSuit);
+
+            
             
         }
 
